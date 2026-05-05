e86eb11e73b6784ba8e6e66d839b32e56502aadf
Yuri <yuri.camargo@sempreceub.com>
2026-05-05 15:28:02 -0300

    Add function to display word and number

---
+++ projetos-academicos/PP5/PP5-1.py
@@ -0,0 +1,6 @@
+def mostra_valor_numero(palavra, numero):
+    print(palavra, numero)
+if __name__== '__main__':
+    word = str(input("Digite uma palavra: "))
+    number = int(input("Digite um número: "))
+    mostra_valor_numero(word, number)
