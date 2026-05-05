78dc6fd771a0e36b8d703009df8c0822636224ec
Yuri <yuri.camargo@sempreceub.com>
2026-05-05 15:14:07 -0300

    Add comparison exercise for two numbers

---
+++ projetos-academicos/EXERCÍCIOS SALA 11-03 if & else pt.2/Exercício2(sala).py
@@ -0,0 +1,8 @@
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+if n1 > n2:
+    print(f"O Maior valor é: ", n1 )
+elif n1 < n2:
+    print("O Maior valor é:", n2)
+else:
+    print("Os valores são iguais!", n1, n2)
