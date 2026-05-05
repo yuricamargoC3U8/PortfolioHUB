4a07f6cf40edd8dd00d76b5938774da111858e0b
Yuri <yuri.camargo@sempreceub.com>
2026-05-05 15:12:59 -0300

    Add number classification and division logic

---
+++ projetos-academicos/EXERCÍCIOS SALA 11-03 if & else pt.2/Exercício1(sala).py
@@ -0,0 +1,11 @@
+valor = int(input('Digite um número: '))
+print(valor % 2)
+if valor % 2 == 0:
+    print('\nPar')
+else:
+    print('\nÍmpar')
+print("O Número dividido por 2 é: ",  valor//2)
+if valor % 5 == 0:
+    print('\nÉ múltiplo de 5')
+else:
+    print('\nNão é múltiplo de 5')
